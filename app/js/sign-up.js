f15e01a9ad1e47492f01cd60b0cff64589673b9a
Justin Searls <searls@gmail.com>
2013-03-13 04:03:41 -0400

    Pull up bound events

--- app/js/sign-up.js
+++ app/js/sign-up.js
@@ -22,50 +22,53 @@ app.Router = Backbone.Router.extend({
 app.SignUpView = Backbone.View.extend({
   template: JST['app/templates/sign-up-form.us'],
 
+  events: {
+    "click button": "createAccount",
+    "change input[name='login']": "checkAvailability",
+    "change input[name='passwordConfirmation']": "confirmPassword"
+  },
+
   render: function(){
     this.$el.html(this.template());
     return this;
+  },
+
+  createAccount: function(e){
+    e.preventDefault();
+    $.post('/accounts', $('.create-account form').serialize());
+  },
+
+  checkAvailability: function(e){
+    var requestedLogin = $(e.target).val();
+    $.get('/account_availability/'+requestedLogin, function(response) {
+      var message = response.available ? "Available!" : "That name's taken :-(";
+      app.ViewHelpers.displayPopover(e.target, message);
+    });
+  },
+
+  confirmPassword: function(e){
+    var password = this.$('input[name="password"]').val(),
+        passwordConfirmation = $(e.target).val();
+
+    if(password !== passwordConfirmation) {
+      app.ViewHelpers.displayPopover(e.target, "Uh oh! Double-check your password!");
+    } else {
+      app.ViewHelpers.clearPopover(e.target);
+    }
   }
 });
 
+app.ViewHelpers = {
+  displayPopover: function(el, message) {
+    $(el).popover('destroy').popover({content: message}).popover('show');
+  },
+
+  clearPopover: function(el) {
+    $(el).popover('destroy');
+  }
+};
 
 jQuery(function($){
   window.router = new app.Router();
   Backbone.history.start();
-
-  // Bind the events
-  $('.create-account button').on('click', function(e){
-    e.preventDefault();
-    $.post('/accounts', $('.create-account form').serialize());
-  });
-
-  //Handle account availability
-  $('.create-account input[name="login"]').on('change', function(e){
-    requestedLogin = $(e.target).val();
-    $.get('/account_availability/'+requestedLogin, function(response) {
-      var message = response.available ? "Available!" : "That name's taken :-(";
-      displayPopover(e.target, message);
-    });
-  });
-
-  //Handle password confirmation
-  $('.create-account input[name="passwordConfirmation"]').on('change', function(e){
-    var password = $('.create-account input[name="password"]').val(),
-        passwordConfirmation = $(e.target).val();
-
-    if(password !== passwordConfirmation) {
-      displayPopover(e.target, "Uh oh! Double-check your password!");
-    } else {
-      clearPopover(e.target);
-    }
-  });
-
-  var displayPopover = function(el, message) {
-    $(el).popover('destroy').popover({content: message}).popover('show');
-  };
-
-  var clearPopover = function(el) {
-    $(el).popover('destroy');
-  };
-
 });
